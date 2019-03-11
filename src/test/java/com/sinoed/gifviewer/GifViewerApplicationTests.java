ebe4763418ee5a934917df3d3ca4a5ffc4e3fe49
Sinoed <cnooed@gmail.com>
2019-03-11 16:21:51 +0330

    first commit

---
+++ src/test/java/com/sinoed/gifviewer/GifViewerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sinoed.gifviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GifViewerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
