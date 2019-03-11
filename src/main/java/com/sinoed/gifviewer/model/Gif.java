851d035932d459ce6d2b2581148d1d80684cb5cc
Sinoed <cnooed@gmail.com>
2019-03-11 17:45:33 +0330

    added Gif and User Entities

--- src/main/java/com/sinoed/gifviewer/model/Gif.java
+++ src/main/java/com/sinoed/gifviewer/model/Gif.java
@@ -1,4 +1,24 @@
 package com.sinoed.gifviewer.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class Gif {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private LocalDate dateUploaded;
+
+    private boolean favorite;
+
+    @ManyToOne
+    private User user;
 }
--- src/main/java/com/sinoed/gifviewer/model/User.java
+++ src/main/java/com/sinoed/gifviewer/model/User.java
@@ -1,4 +1,21 @@
 package com.sinoed.gifviewer.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Gif> gifs = new HashSet<>();
 }
