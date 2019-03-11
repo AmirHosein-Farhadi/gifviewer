034916bb3332b15b81bfe87de8db1000acfa142f
Sinoed <cnooed@gmail.com>
2019-03-11 18:08:32 +0330

    injected the repository into controller

--- src/main/java/com/sinoed/gifviewer/controller/GifController.java
+++ src/main/java/com/sinoed/gifviewer/controller/GifController.java
@@ -1,12 +1,22 @@
 package com.sinoed.gifviewer.controller;
 
+import com.sinoed.gifviewer.repository.GifRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GifController {
+
+    private final GifRepository gifRepository;
+
+    public GifController(GifRepository gifRepository) {
+        this.gifRepository = gifRepository;
+    }
+
     @RequestMapping("/")
-    public String homePage(){
+    public String homePage(Model model){
+
         return "home";
     }
 }
