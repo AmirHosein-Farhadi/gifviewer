ba0fe0e04d510281c8d70c3be769d665187c52f6
Sinoed <cnooed@gmail.com>
2019-03-12 16:27:04 +0330

    added repositories

--- src/main/java/com/sinoed/gifviewer/repository/GifRepository.java
+++ src/main/java/com/sinoed/gifviewer/repository/GifRepository.java
@@ -3,9 +3,5 @@ package com.sinoed.gifviewer.repository;
 import com.sinoed.gifviewer.model.Gif;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface GifRepository extends CrudRepository<Gif,Long> {
-
-    Optional<Gif> findByName(String name);
 }
--- src/main/java/com/sinoed/gifviewer/repository/UserRepository.java
+++ src/main/java/com/sinoed/gifviewer/repository/UserRepository.java
@@ -3,8 +3,5 @@ package com.sinoed.gifviewer.repository;
 import com.sinoed.gifviewer.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends CrudRepository<UserRepository,Long> {
-    Optional<User> findByName(String name);
+public interface UserRepository extends CrudRepository<User,Long> {
 }
