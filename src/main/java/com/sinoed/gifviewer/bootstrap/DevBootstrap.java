fab487d5a47833f4f74f6ffda6619d29a956b403
Sinoed <cnooed@gmail.com>
2019-03-12 16:28:19 +0330

    added bootstrap and inserted some testing data

--- src/main/java/com/sinoed/gifviewer/bootstrap/DevBootstrap.java
+++ src/main/java/com/sinoed/gifviewer/bootstrap/DevBootstrap.java
@@ -1,10 +1,26 @@
 package com.sinoed.gifviewer.bootstrap;
 
+import com.sinoed.gifviewer.model.Gif;
 import com.sinoed.gifviewer.model.User;
+import com.sinoed.gifviewer.repository.GifRepository;
+import com.sinoed.gifviewer.repository.UserRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
+@Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final GifRepository gifRepository;
+    private final UserRepository userRepository;
+
+    public DevBootstrap(GifRepository gifRepository, UserRepository userRepository) {
+        this.gifRepository = gifRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         initData();
@@ -12,5 +28,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
         User user = new User("Amir");
+        LocalDate localDate = LocalDate.of(1999,2,4);
+        Gif gif = new Gif("gif",localDate,true, user);
+
+        user.getGif().add(gif);
+
+
+        userRepository.save(user);
+        gifRepository.save(gif);
+
     }
 }
